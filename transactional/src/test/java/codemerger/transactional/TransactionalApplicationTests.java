992b1743b0b33a301edd98e8a0462563d5d49737
Michael Bielang <michael.bielang@hs-augsburg.de>
2020-12-13 14:17:23 +0100

    init transaction module

---
+++ transactional/src/test/java/codemerger/transactional/TransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package codemerger.transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

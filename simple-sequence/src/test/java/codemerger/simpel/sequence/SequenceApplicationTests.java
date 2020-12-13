6ec8a1e7f534ed1dae3fa421abca8a9362e9c5a1
Michael Bielang <michael.bielang@hs-augsburg.de>
2020-12-13 14:00:01 +0100

    init simple sequence

---
+++ simple-sequence/src/test/java/codemerger/simpel/sequence/SequenceApplicationTests.java
@@ -0,0 +1,13 @@
+package codemerger.simpel.sequence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

014a316fcd4ab7174b9969f74a40cddd131203c0
Michael Bielang <michael.bielang@hs-augsburg.de>
2020-12-20 14:26:16 +0100

    add h2 web-access

--- jpa-testing/src/main/java/codemerger/jpa/test/evenhandler/ApplicationEventHandler.java
+++ jpa-testing/src/main/java/codemerger/jpa/test/evenhandler/ApplicationEventHandler.java
@@ -8,6 +8,8 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import static java.lang.Thread.sleep;
+
 /**
  * Organisation: Codemerger Ldt.
  * Project: jpa
@@ -33,5 +35,6 @@ public class ApplicationEventHandler {
 
         final Person resultPerson = personRepository.findByEmail("1");
         System.out.println(resultPerson);
+        sleep(500000000);
     }
 }
